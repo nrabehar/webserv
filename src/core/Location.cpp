a2f13fb0b49ed83463f08315adca5d3b93b10e43
nrabarij <nrabarij@student.42antananarivo.mg>
2025-08-20 21:38:19 +0300

    feat: add Location class

---
+++ src/core/Location.cpp
@@ -0,0 +1,41 @@
+#include "Location.hpp"
+
+Location::~Location()
+{
+  DBG(METHOD_NAME + " called.");
+}
+
+Location::Location()
+{
+  DBG(METHOD_NAME + " called.");
+}
+
+const std::vector<std::string> & Location::get(const std::string & key) const
+{
+  static const std::vector<std::string> __empty_vector;
+  DBG(METHOD_NAME + " called.");
+  if (key.empty())
+  {
+    WRN("Key is empty.");
+    return (__empty_vector);
+  }
+  std::map<std::string, std::vector<std::string>>::const_iterator it;
+  it = _data.find(key);
+  if (it != _data.end())
+  {
+    return (it->second);
+  }
+  WRN("Key not found: " + key);
+  return (__empty_vector);
+}
+
+void Location::set(const std::string & k, const std::vector<std::string> & v)
+{
+  DBG(METHOD_NAME + " called.");
+  if (k.empty())
+  {
+    WRN("Key is empty, cannot set value.");
+    return ;
+  }
+  _data.insert(std::make_pair(k, v));
+}
---
+++ src/core/Location.hpp
@@ -0,0 +1,21 @@
+#ifndef LOCATION_HPP
+#define LOCATION_HPP 1
+
+#include <map>
+#include <vector>
+
+class Location
+{
+  private:
+    std::map<std::string, std::vector<std::string>> _data;
+  public:
+    Location();
+    ~Location();
+    const std::vector<std::string> & get(const std::string &) const;
+    void set(const std::string &, const std::vector<std::string> &);
+  private:
+    Location(const Location &);
+    Location & operator=(const Location &);
+};
+
+#endif // LOCATION_HPP
