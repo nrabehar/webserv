c0ba3be464b114ec31f8ebaa3dd9106fdf3f1e31
Niriniavo Safidy Nekena Rabeharivonjy <nrabehar@c1r4s11.42antananarivo.mg>
2025-10-17 23:40:15 +0300

    fix: error handler

--- src/handler/ErrorHandler.cpp
+++ src/handler/ErrorHandler.cpp
@@ -35,27 +35,61 @@ void	ErrorHandler::serveCustomPage(const std::string & path, Http::Response & re
 {
 
 	_handler->setStatus(HS_WAITING);
-	StaticHandler * static_handler = new StaticHandler(_handler);
-	if (!static_handler->handle(path, &res))
+	std::ifstream file(path.c_str(), std::ios::in | std::ios::binary);
+	if (!file.good())
+		return (loadHtmlErrorPage(res.status(), res));
+	std::list<std::string> contents;
+	char buffer[4096];
+	while (file.read(buffer, sizeof(buffer)))
+		contents.push_back(std::string(buffer, sizeof(buffer)));
+	contents.push_back(std::string(buffer, file.gcount()));
+	file.close();
+	size_t total_size = 0;
+	for (std::list<std::string>::iterator it = contents.begin(); it != contents.end(); ++it)
+		total_size += it->size();
+	res.setHeader("Content-Length", String::str(total_size));
+	std::string headers = res.str();
+	total_size += headers.size() + 1;
+	try
 	{
-		delete static_handler;
+		size_t offset = 0;
+		char * buf = new char[total_size];
+		std::memset(buf, 0, total_size);
+		std::memmove(buf, headers.c_str(), headers.size());
+		offset += headers.size();
+		for (std::list<std::string>::iterator it = contents.begin(); it != contents.end(); ++it)
+		{
+			std::memmove(buf + offset, it->c_str(), it->size());
+			offset += it->size();
+		}
+		_handler->client()->setOut(buf, total_size);
+	}
+	catch(const std::exception& e)
+	{
+		(void)e;
+		ERR("Error while preparing file content: " + std::string(e.what()));
 		return (loadHtmlErrorPage(res.status(), res));
 	}
-	if (static_handler->fd() != IN_MEMORY_FD)
-	{
-		return (EventLoop::instance().addHandler(static_handler, POLLIN));
-	}
-	delete static_handler;
-	_handler->setStatus(HS_OK);
 }
 
 void	ErrorHandler::loadHtmlErrorPage(int status, Http::Response & res)
 {
 
 	std::string body = UI::getErrorPage(status, res.reason(status));
+	res.setHeader("Content-Length", String::str(body.length()));
 
-	res.appendBody(body);
-	res.setHeader("Content-Length", String::str(res.body().length()));
+	size_t total_size = 0;
+	std::string headers = res.str();
+	total_size += headers.size() + 1;
+	total_size += body.size();
+	char * buf = new char[total_size];
+	std::memset(buf, 0, total_size);
+	size_t offset = 0;
+	std::memmove(buf, headers.c_str(), headers.size());
+	offset += headers.size();
+	std::memmove(buf + offset, body.c_str(), body.size());
+	offset += body.size();
+	_handler->client()->setOut(buf, total_size);
 	_handler->setStatus(HS_OK);
 
 }
